72dc8ebf8833bcfcd322f67addd7846812ade0a8
Kirill Snopko <kirillsnopko@gmail.com>
2023-05-12 14:46:47 +0300

    remove unused

--- src/main/java/com/snopko/RestApi/RestApiApplication.java
+++ src/main/java/com/snopko/RestApi/RestApiApplication.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @SpringBootApplication
 @EnableWebSecurity
 public class RestApiApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(RestApiApplication.class, args);
     }
